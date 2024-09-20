a7fb0c47c5daf68b99a1859202324373ee7c787e
Henry Wallace <67589487+henry-wallace-phys@users.noreply.github.com>
2024-09-20 10:57:36 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,8 @@ release = 'v1.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc'
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
