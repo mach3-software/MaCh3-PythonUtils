d230da9517d7df8ed2b8d07b3514c504f125293c
Henry Wallace <henryi@beluga2.int.ets1.calculquebec.ca>
2024-10-31 11:20:51 -0400

    Update cfg reader

--- src/MaCh3PythonUtils/config_reader/config_reader.py
+++ src/MaCh3PythonUtils/config_reader/config_reader.py
@@ -15,8 +15,7 @@ from MaCh3PythonUtils.fitters.multi_mcmc_gpu import MCMCMultGPU
 
 from deepmerge import always_merger
 
-class ConfigReader:    
-    
+class ConfigReader:
     # Strictly unecessary but nice conceptually
     _file_handler = None
     _interface    = None
@@ -256,11 +255,14 @@ class ConfigReader:
                 self._interface.run_likelihood_scan(self.__chain_settings["LikelihoodScanSettings"]["NDivisions"])
                 
             if self.__chain_settings["FileSettings"]["RunMCMC"] and self._interface is not None:
+                
+                print("WARNING: MCMC HAS ONLY BEEN TESTED WITH TENSORFLOW INTERFACES!")
 
                 mcmc = MCMCMultGPU(self._interface,
                         self.__chain_settings["MCMCSettings"]["NChains"],
                         self.__chain_settings["ParameterSettings"]["CircularParameters"],
                         self.__chain_settings["MCMCSettings"]["UpdateStep"])
+
 
                 mcmc(self.__chain_settings["MCMCSettings"]["NSteps"],
                      self.__chain_settings["MCMCSettings"]["MCMCOutput"])
